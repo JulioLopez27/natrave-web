09cf012ebfef21009aa4d95ce27c49fea72768a8
Julio Lopez <julioneo95@hotmail.com>
2024-04-16 00:27:50 -0300

    Upd: implementation of new modal logic for errors

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,7 +3,8 @@ import { useFormik } from 'formik'
 import * as yup from 'yup'
 import { useLocalStorage } from 'react-use'
 import { Navigate } from 'react-router-dom'
-import { Icon, Input, errorEmailLogin,errorPassLogin,fails } from '~/components'
+import { useState } from 'react'
+import { Icon, Input, CustomModal } from '~/components'
 
 
 
@@ -17,6 +18,9 @@ const validationSchema = yup.object().shape({
 export const Login = () => {
 
     const [auth, setAuth] = useLocalStorage('auth', {})
+    const [isModalOpen, setIsModalOpen] = useState(false) // Agrega este estado
+    const [message, setMessage] = useState('') // Agrega este estado para manejar el mensaje de error  
+    const [messageType, setMessageType] = useState('')
 
     const formik = useFormik({
         onSubmit: async (values) => {
@@ -32,13 +36,13 @@ export const Login = () => {
                 })
                 setAuth(res.data)
             } catch (error) {
-                if (error.response.status === 404) {
-                    errorEmailLogin()
-                }else if(error.response.status === 406){
-                    errorPassLogin()
-                }else{fails()}
+                if (error.response.status === 404 || error.response.status === 406) {
+                    setIsModalOpen(true);
+                    setMessage("The information you entered is not correct.");
+                    setMessageType('error');
+                    return;
+                }
             }
-
         },
 
         initialValues: {
@@ -52,6 +56,11 @@ export const Login = () => {
     if (auth?.user?.id) {
         return <Navigate to="/dashboard" replace={true} />
     }
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false); // Función para cerrar el modal
+    }
+
 
     return (
         <div>
@@ -68,36 +77,41 @@ export const Login = () => {
                     </a>
                     <h2 className="text-xl font-bold">Login into your account</h2>
                 </div>
+                <section>
 
-                <form className="p-4 space-y-6" onSubmit={formik.handleSubmit}>
-                    <Input
-                        type="text"
-                        name="email"
-                        label="Your e-mail"
-                        placeholder="Type your e-mail"
-                        error={formik.touched.email && formik.errors.email}
-                        value={formik.values.email}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                    />
-                    <Input
-                        type="password"
-                        name="password"
-                        label="Your password"
-                        placeholder="Type your password"
-                        error={formik.touched.password && formik.errors.password}
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                    />
+                    <form className="p-4 space-y-6" onSubmit={formik.handleSubmit}>
+                        <Input
+                            type="text"
+                            name="email"
+                            label="Your e-mail"
+                            placeholder="Type your e-mail"
+                            error={formik.touched.email && formik.errors.email}
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                        />
+                        <Input
+                            type="password"
+                            name="password"
+                            label="Your password"
+                            placeholder="Type your password"
+                            error={formik.touched.password && formik.errors.password}
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                        />
 
-                    <button type='submit' className="block w-full text-center text-white bg-red-500  px-6 py-3 rounded-xl disabled:opacity-50"
-                        disabled={!formik.isValid || formik.isSubmitting}>
-                        {formik.isSubmitting ? 'Loading...' : 'Login'}
-                    </button>
+                        <button type='submit' className="block w-full text-center text-white bg-red-500  px-6 py-3 rounded-xl disabled:opacity-50"
+                            disabled={!formik.isValid || formik.isSubmitting}>
+                            {formik.isSubmitting ? 'Loading...' : 'Login'}
+                        </button>
 
-                </form>
-
+                    </form>
+                </section>
+                <CustomModal isOpen={isModalOpen} onClose={handleCloseModal} message={message} messageType={messageType}>
+                    <h2 className="text-2xl">{messageType === 'error' ? 'Opss..' : 'Success'}</h2>
+                    {messageType === 'error' && <p>{message}</p>}
+                </CustomModal>
             </main>
 
         </div>
